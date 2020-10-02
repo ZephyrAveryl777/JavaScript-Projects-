66e7c69496cc50b0602d2e6762ae0cddf9fcdbc3
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2020-10-02 16:54:25 +0530

    files added

---
+++ full-screen-video/app.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+btn.addEventListener("click", () => {
+    if (!btn.classList.contains("slide")) {
+        btn.classList.add("slide");
+        video.pause();
+    } else {
+        btn.classList.remove("slide");
+        video.play();
+    }
+});
+
+
+// preloader
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", setTimeout(() => {
+    preloader.classList.add("hide-preloader");
+}, 3000));

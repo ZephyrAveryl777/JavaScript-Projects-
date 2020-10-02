332bfb2d35852cfc8d4c08ee2f326652c9faf22b
Zephyr Averyl <67591915+ZephyrAveryl777@users.noreply.github.com>
2020-10-02 16:45:38 +0530

    files for stopwatch added

---
+++ Stopwatch/app.js
@@ -0,0 +1,43 @@
+var min = 0;
+var sec = 0;
+var miliSec = 0;
+var timer;
+
+function callTimer() {
+    miliSec++;
+    if (miliSec < 100) {
+        if (miliSec === 99) {
+            miliSec = 0;
+            sec++;
+            if (sec === 60)  {
+                sec = 0;
+                min++;
+            }
+        }
+    }
+    else{
+        miliSec = 0;
+    }
+    document.getElementById("timer").innerHTML = (min < 10 ? "0" + min:min) 
+    + ":" + (sec < 10 ? "0" + sec : sec) + ":" + (miliSec < 10 ? "0" + miliSec : miliSec );
+}
+
+
+function start() {
+    document.getElementById("start").disabled = true;
+    timer = setInterval(callTimer, 10);
+}
+
+function stop() {
+    document.getElementById("start").disabled = false;
+    clearInterval(timer);
+}
+
+function reset() {
+    stop();
+    min = 0;
+    sec = 0;
+    miliSec = 0;
+    document.getElementById("timer").innerHTML = (min < 10 ? "0" + min:min) 
+    + ":" + (sec < 10 ? "0" + sec : sec) + ":" + (miliSec < 10 ? "0" + miliSec : miliSec );
+}
